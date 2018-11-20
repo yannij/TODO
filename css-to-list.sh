6895dbdba6873bff0c0e977dd4e62b247b514fce
Yanni Chiu <yannix7db@gmail.com>
2018-11-20 01:39:45 -0500

    small fix

--- css-to-list.sh
+++ css-to-list.sh
@@ -4,7 +4,7 @@ grep '^\.mdc-' ~/foo \
 | awk -F'+' '{for (i = 0; ++i <= NF;) print $i}' \
 | awk -F'>' '{for (i = 0; ++i <= NF;) print $i}' \
 | awk -F'.' '{for (i = 0; ++i <= NF;) print $i}' \
-| grep '^\mdc-' \
+| grep '^mdc-' \
 | sed -e 's/[:\[].*//' \
 | sed -e 's/[~)]$//' \
 | sort -u
