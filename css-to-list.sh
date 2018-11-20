bc8e3b1c12c686bc33addf7eb45486c9c64812c3
Yanni Chiu <yannix7db@gmail.com>
2018-11-20 01:35:15 -0500

    add Material Components Web css class extraction script

---
+++ css-to-list.sh
@@ -0,0 +1,10 @@
+grep '^\.mdc-' ~/foo \
+| awk -F' ' '{for (i = 0; ++i <= NF;) print $i}' \
+| awk -F',' '{for (i = 0; ++i <= NF;) print $i}' \
+| awk -F'+' '{for (i = 0; ++i <= NF;) print $i}' \
+| awk -F'>' '{for (i = 0; ++i <= NF;) print $i}' \
+| awk -F'.' '{for (i = 0; ++i <= NF;) print $i}' \
+| grep '^\mdc-' \
+| sed -e 's/[:\[].*//' \
+| sed -e 's/[~)]$//' \
+| sort -u
